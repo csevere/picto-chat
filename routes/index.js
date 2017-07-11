c8acaa7631474251ef885a163c4855a6150edf34
Carlita <carla.severe@gmail.com>
2017-07-11 15:23:53 -0400

    changed port

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var express = require('express'),
     //io is listening to the server
     io = require('socket.io').listen(server);
 	//set a port for the app to run on
-	server.listen(3000);
+	server.listen(3001);
 	console.log("listening on port 7090")
 
 	//import delivery and fs modules for file transfer, but not necessary for express however
